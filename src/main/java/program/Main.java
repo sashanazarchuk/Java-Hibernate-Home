4c6e6e76c4d56b353672a052423581e1876b47fa
Oleksander Nazarchuk <ss.nazarchuk2@gmail.com>
2023-02-11 09:26:00 +0200

    Added possibility of passing the test and issuing an estimate

--- src/main/java/program/Main.java
+++ src/main/java/program/Main.java
@@ -18,39 +18,91 @@ public class Main {
         //testRole();
         try {
             Scanner input = new Scanner(System.in);
-            System.out.println("Введіть запитання");
-            String quest = input.nextLine();
-            addQuestion(quest, QuestionType.RADIO_BUTTON);
-
-            int id = 0;
-            Session context = HiberContext.getSessionFactory().openSession();
-            Query query = context.createQuery("FROM Question");
-            List<Question> questions = query.list();
-            for (Question question : questions)
-                id = question.getId();
-
-            String answer = "";
-            boolean isCorrect = false;
-            for (int i = 0; i < 3; i++) {
-                System.out.println("Введіть відповідь");
-                answer = input.nextLine();
-                System.out.println("Це вірна відповідь? y/n");
-                String isCorrectStr = input.nextLine();
-                if (isCorrectStr == "y")
-                    isCorrect = true;
-                else
-                    isCorrect = false;
-                addQuestionItem(id, answer, isCorrect);
+            boolean cycle = true;
+            while (cycle) {
+                System.out.println("Виберіть дію \n 1)Пройти тест \n 2)Додати нове запитання \n 3)Вийти");
+                int action = Integer.parseInt(input.nextLine());
+                switch (action) {
+                    case 1:
+                        passTest();
+                        break;
+                    case 2:
+                        addConsoleQuestion();
+                        break;
+                    case 3:
+                        cycle = false;
+                        break;
+                    default:
+                        break;
+                }
             }
-
-
-            context.close(); // закриває підключення
-
         } catch (Exception ex) {
-            System.out.println("Виникла помилка" + ex.getMessage()); //видиться при помилці
+            System.out.println("Виникла помилка" + ex.getMessage()); //виводиться при помилці
         }
     }
 
+
+    //метод для проходження тесту
+    private static void passTest() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        Session context = HiberContext.getSessionFactory().openSession();
+        Query query = context.createQuery("FROM Question");
+        List<Question> questions = query.list();
+        int questionCount = 0;
+        int counter = 0;
+        for (Question question : questions) {
+            System.out.println(question.getText());
+
+            Query query2 = context.createQuery("FROM QuestionItem where question.id = " + question.getId());
+            List<QuestionItems> answers = query2.list();
+            int i = 1;
+            int correctAnsw = 0;
+            for (QuestionItems answer : answers) {
+                System.out.println(i + ")" + answer.getText());
+                if (answer.isTrue() == true)
+                    correctAnsw = i;
+                i++;
+            }
+            System.out.println("Введіть відповідь");
+            int answ = Integer.parseInt(input.nextLine());
+            if (answ == correctAnsw) {
+                counter++;
+            }
+            questionCount++;
+        }
+        System.out.println("Your result is " + counter + "/" + questionCount);
+        context.close();
+    }
+
+    //метод для додавання питань і відповідей в бд
+    private static void addConsoleQuestion() throws SQLException {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введіть запитання");
+        String quest = input.nextLine();
+        addQuestion(quest, QuestionType.RADIO_BUTTON);
+
+        int id = 0;
+        Session context = HiberContext.getSessionFactory().openSession();
+        Query query = context.createQuery("FROM Question");
+        List<Question> questions = query.list();
+        for (Question question : questions)
+            id = question.getId();
+
+        String answer = "";
+        boolean isCorrect = false;
+        for (int i = 0; i < 3; i++) {
+            System.out.println("Введіть відповідь");
+            answer = input.nextLine();
+            System.out.println("Це вірна відповідь? y/n");
+            String isCorrectStr = input.nextLine();
+            if (isCorrectStr == "y")
+                isCorrect = true;
+            else
+                isCorrect = false;
+            addQuestionItem(id, answer, isCorrect);
+        }
+        context.close(); // закриває підключення
+    }
 
     //метод для додавання питання в бд
     private static void addQuestion(String text, QuestionType type) throws SQLException {
